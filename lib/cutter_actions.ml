ac2ce7f4ed520d65946108c3c640615a68f7426d
Eduardo Costa <ecosta@hubspot.com>
2022-02-12 15:17:47 +0000

    cutter: moving code around

--- lib/cutter_actions.ml
+++ lib/cutter_actions.ml
@@ -2,51 +2,9 @@ open Cutter_data
 
 let tree : Atoms.t list ref = ref []
 
-let i32 bs n = Bytes.get_int32_be bs n |> Int32.to_int
-
-let header_of_mdhd mvhd : md_duration = 
-  let scale = i32 mvhd 12 |> MediaScale.of_int in
-  let value = i32 mvhd 16 in
-  { scale; value }
-
-let header_of_mvhd mvhd : mv_duration = 
-  let scale = i32 mvhd 12 |> MovieScale.of_int in
-  let value = i32 mvhd 16 in
-  { scale; value }
-
-let edit_of_elst (mvhd : mv_duration) (mdhd : md_duration) elst : edit list =
-  let entry x : edit =
-    let p = 8 + x * 12 in
-    let dur = i32 elst p in
-    let mtime = i32 elst (p + 4) in
-    let mrate = (float_of_int (i32 elst (p + 8))) /. 65536.0 in
-    {
-      dur = { value = dur; scale = mvhd.scale };
-      mtime = { value = mtime; scale = mdhd.scale };
-      mrate 
-    }
-  in
-  let rec entries = function
-    | 0 -> []
-    | x -> entry (x - 1) :: entries (x - 1)
-  in
-  let qty = i32 elst 4 in
-  entries qty |> List.rev
-
-let duration_of_tkhd mvhd tkhd : mv_duration = 
-  let v = i32 tkhd 20 in
-  MovieDuration.with_value v mvhd
-
-let track_of_trak mvhd trak : track =
-  let tkhd = Atoms.find_leaf_atom "tkhd" trak |> duration_of_tkhd mvhd in
-  let mdhd = Atoms.find_node_atom "mdia" trak |> Atoms.find_leaf_atom "mdhd" |> header_of_mdhd in
-  let edts = Atoms.find_node_atom "edts" trak |> Atoms.find_leaf_atom "elst" |> edit_of_elst mvhd mdhd in
-  { tkhd; mdhd; edts }
-
-let movie_from_moov moov : movie =
-  let mvhd = Atoms.find_leaf_atom "mvhd" moov |> header_of_mvhd in
-  let traks = Atoms.find_all_node_atoms "trak" moov |> List.map (track_of_trak mvhd) in
-  { mvhd; traks }
+let map_tree fn =
+  let m = Atoms.find_node_atom "moov" !tree |> Cutter_parser.moov |> fn in
+  Cutter_debug.movie m
 
 let add_md_dur (dur : md_duration) secs = 
   let fn d s = d + (secs * s) in
@@ -57,8 +15,7 @@ let add_mv_dur (dur : mv_duration) secs =
 
 let load file =
   tree := Atoms.from_file file;
-  let m = Atoms.find_node_atom "moov" !tree |> movie_from_moov in
-  Cutter_debug.movie m
+  map_tree (fun x -> x)
 
 let ltrim trk edt secs =
   let chg_edit i e : edit =
@@ -81,8 +38,7 @@ let ltrim trk edt secs =
     let mvhd = List.map dur_of traks |> MovieDuration.max_of in
     { mvhd; traks }
   in
-  let m = Atoms.find_node_atom "moov" !tree |> movie_from_moov |> chg_movie in
-  Cutter_debug.movie m
+  map_tree chg_movie
 
 let play () =
   Atoms.to_file "/tmp/m4c0.cutter.test.mov" !tree;
---
+++ lib/cutter_parser.ml
@@ -0,0 +1,45 @@
+open Cutter_data
+
+let i32 bs n = Bytes.get_int32_be bs n |> Int32.to_int
+
+let elst (mvhd : MovieDuration.t) (mdhd : MediaDuration.t) (bs : bytes) : edit list =
+  let entry (idx : int) : edit =
+    let p = 8 + idx * 12 in
+    let dur_raw = i32 bs p in
+    let dur = MovieDuration.with_value dur_raw mvhd in
+    let mtime_raw = i32 bs (p + 4) in
+    let mtime = MediaDuration.with_value mtime_raw mdhd in
+    let mrate = (float_of_int (i32 bs (p + 8))) /. 65536.0 in
+    { dur; mtime; mrate }
+  in
+  let rec entries = function
+    | 0 -> []
+    | x -> entry (x - 1) :: entries (x - 1)
+  in
+  i32 bs 4 |> entries |> List.rev
+
+let mdhd (bs : bytes) : MediaDuration.t =
+  let scale = i32 bs 12 |> MediaScale.of_int in
+  let value = i32 bs 16 in
+  { scale; value }
+
+let mvhd (bs : bytes) : MovieDuration.t = 
+  let scale = i32 bs 12 |> MovieScale.of_int in
+  let value = i32 bs 16 in
+  { scale; value }
+
+let tkhd (mvhd : MovieDuration.t) (bs : bytes) : MovieDuration.t =
+  let v = i32 bs 20 in
+  MovieDuration.with_value v mvhd
+
+let trak (mvhd : MovieDuration.t) (moov : Atoms.t list) : track =
+  let tkhd = Atoms.find_leaf_atom "tkhd" moov |> tkhd mvhd in
+  let mdhd = Atoms.find_node_atom "mdia" moov |> Atoms.find_leaf_atom "mdhd" |> mdhd in
+  let edts = Atoms.find_node_atom "edts" moov |> Atoms.find_leaf_atom "elst" |> elst mvhd mdhd in
+  { tkhd; mdhd; edts }
+
+let moov (moov : Atoms.t list) : movie =
+  let mvhd = Atoms.find_leaf_atom "mvhd" moov |> mvhd in
+  let traks = Atoms.find_all_node_atoms "trak" moov |> List.map (trak mvhd) in
+  { mvhd; traks }
+
